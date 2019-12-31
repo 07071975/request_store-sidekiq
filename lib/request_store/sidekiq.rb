614b2e9bf5e0126df67b98223a8c68e7f083415d
Dmitrij.mdl <dmitrij.mdl@gmail.com>
2019-12-31 11:29:18 +0300

    added request_id

--- lib/request_store/sidekiq.rb
+++ lib/request_store/sidekiq.rb
@@ -3,12 +3,21 @@ require "sidekiq"
 
 require "request_store/sidekiq/version"
 require "request_store/sidekiq/server_middleware"
+require "request_store/sidekiq/client_middleware"
 require "request_store/sidekiq/railtie" if defined?(Rails::Railtie)
 
 module RequestStore
   module Sidekiq
     def self.add_custom_middleware!
+      ::Sidekiq.configure_client do |config|
+        config.client_middleware do |chain|
+          chain.add ClientMiddleware
+        end
+      end
       ::Sidekiq.configure_server do |config|
+        config.client_middleware do |chain|
+          chain.add ClientMiddleware
+        end
         config.server_middleware do |chain|
           chain.add ServerMiddleware
         end
--- lib/request_store/sidekiq/server_middleware.rb
+++ lib/request_store/sidekiq/server_middleware.rb
@@ -2,9 +2,23 @@ module RequestStore
   module Sidekiq
     class ServerMiddleware
       def call(worker, job, queue)
+        if job.key?('request_id')
+          set_request_id(job['request_id'])
+        end
+        if job.key?('psp_id')
+          set_psp_id(job['psp_id'])
+        end
         yield
-      ensure
-        ::RequestStore.clear!
+      end
+
+      private
+
+      def set_request_id(request_id)
+        RequestStore.store[:request_id] = request_id
+      end
+
+      def set_psp_id(psp_id)
+        RequestStore.store[:psp_id] = psp_id
       end
     end
   end
